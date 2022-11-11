09982c29525bf4b26eb251aa158844e552af0217
Coder Thinker <ypl123698745@qq.com>
2022-11-11 19:31:25 +0800

    first commit

---
+++ include/GL_Head.h
@@ -0,0 +1,14 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+class count{
+private:
+    int count = 1;
+public:
+    int getCount(){
+        return count++;
+    }
+
+};
